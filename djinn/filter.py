77b867177d32480d4ac24f8d8a42956801037c7c
pdimens <pdimens@gmail.com>
2025-10-08 17:09:00 -0400

    start adding singleton filtering

--- djinn/filter.py
+++ djinn/filter.py
@@ -8,15 +8,17 @@ import subprocess
 from djinn.utils.file_ops import print_error, validate_fq_sam, which_linkedread, which_linkedread_sam
 from djinn.utils.fq_tools import FQRecord, CachedWriter
 
-@click.command(panel = "Other Tools", no_args_is_help = True, epilog = "Documentation: https://pdimens.github.io/djinn/isolate/")
+@click.command(panel = "Other Tools", no_args_is_help = True, epilog = "Documentation: https://pdimens.github.io/djinn/filter/")
 @click.option("-c", "--cache-size", hidden=True, type=click.IntRange(min=1000, max_open=True), default=5000, help = "Number of cached reads for write operations")
 @click.option("-i", "--invalid", is_flag=True, default=False, help = "Separately output records with invalid barcodes")
 @click.option("-t", "--threads", type = click.IntRange(min = 2, max_open=True), default=4, show_default=True, help = "Number of threads to use")
 @click.argument('prefix', required=True, type = str)
 @click.argument('inputs', required=True, type=click.Path(exists = True,dir_okay=False,readable=True,resolve_path=True), callback = validate_fq_sam, nargs=-1)
-def isolate(prefix, inputs, cache_size, invalid, threads):
+def filter_valid(prefix, inputs, cache_size, invalid, threads):
     '''
-    Isolate valid-barcoded reads from invalid ones.
+    Isolate valid-barcoded reads from invalid ones
+
+    Use `--invalid` to separately output reads with invalid barcodes.
     '''
     lr_type = which_linkedread_sam(inputs[0]) if len(inputs) == 1 else which_linkedread(inputs[0])
 
--- djinn/separate_singletons.py
+++ djinn/separate_singletons.py
@@ -1,92 +1,112 @@
 #! /usr/bin/env python
 
+from collections import Counter
 import os
 import re
-import sys
-import argparse
 import subprocess
+import sys
 import pysam
 import rich_click as click
+from djinn.utils.barcodes import ANY_INVALID, TELLSEQ_STLFR_RX
+from djinn.utils.file_ops import print_error, validate_fq_sam, which_linkedread, which_linkedread_sam
+from djinn.utils.fq_tools import FQRecord, CachedWriter
 
-@click.command(panel = "Other Tools", no_args_is_help = True, epilog = "Documentation: https://pdimens.github.io/djinn/isolate/")
+def count_barcodes_sam(bamfile: str, invalid: bool = False) -> Counter:
+    '''
+    Return a Counter of the unique barcodes in bamfile. If invalid is True,
+    the Counter includes invalid barcodes as well.
+    '''
+    barcodes = Counter()
+    with pysam.AlignmentFile(bamfile, check_sq=False) as infile:
+        for record in infile:
+            _bc = None
+            if record.has_tag("BX"):
+                _bc = record.get_tag("BX")
+            else:
+                _check =  TELLSEQ_STLFR_RX.search(record.name)
+                if _check:
+                    _bc = _check.group(0)[1:]
+
+            if _bc:
+                if invalid:
+                    if ANY_INVALID.search(_bc):
+                        barcodes[_bc] += 1
+                else:
+                    barcodes[_bc] += 1
+    return barcodes
+
+def count_barcodes_fq(barcode_type: str, fq1: str, fq2: str, invalid: bool = False) -> Counter:
+    '''
+    Return a Counter of the unique barcodes in the fastq files. If invalid is True,
+    the Counter includes invalid barcodes as well.
+    '''
+    barcodes = Counter()
+    with (
+        pysam.FastxFile(fq1, persist=False) as R1,
+        pysam.FastxFile(fq2, persist=False) as R2,
+    ):
+        for _r1 in R1:
+            _r1 = FQRecord(_r1, True, barcode_type, 0)
+            if invalid:
+                if not _r1.valid:
+                    barcodes[_r1.barcode] += 1
+            else:
+                barcodes[_r1.barcode] += 1
+        for _r2 in R2:
+            _r2 = FQRecord(_r2, False, barcode_type, 0)
+            if invalid:
+                if not _r2.valid:
+                    barcodes[_r2.barcode] += 1
+            barcodes[_r2.barcode] += 1
+    
+    return barcodes
+
+@click.command(panel = "Other Tools", no_args_is_help = True, epilog = "Documentation: https://pdimens.github.io/djinn/filter/")
 @click.option("-c", "--cache-size", hidden=True, type=click.IntRange(min=1000, max_open=True), default=5000, help = "Number of cached reads for write operations")
-@click.option("-i", "--invalid", is_flag=True, default=False, help = "Separately output records with invalid barcodes")
+@click.option("-s", "--singletons", is_flag=True, default=False, help = "Separately output records with valid singleton barcodes")
 @click.option("-t", "--threads", type = click.IntRange(min = 2, max_open=True), default=4, show_default=True, help = "Number of threads to use")
 @click.argument('prefix', required=True, type = str)
 @click.argument('inputs', required=True, type=click.Path(exists = True,dir_okay=False,readable=True,resolve_path=True), callback = validate_fq_sam, nargs=-1)
-def isolate(prefix, inputs, cache_size, invalid, threads):
+def filter_singletons(prefix, inputs, cache_size, singletons, threads):
+    '''
+    Isolate linked reads from singletons
+
+    Use `--singleton` to separately output reads with singleton barcodes.
+    '''
+    if len(inputs) == 1:
+        bc_counts = count_barcodes_sam(inputs[0])
+        linked = filter(lambda x: bc_counts[x] > 2, bc_counts.keys())
+        if singletons:
+            _singles = list(filter(lambda x: bc_counts[x] <= 2, bc_counts.keys()))
+        
+        with pysam.AlignmentFile(inputs[0]) as xam, pysam.AlignmentFile(f"{prefix}.bam", "wb", template = xam) as xam_out:
+            for record in xam:
+                #SEARCH FOR BARCODE
+
+                # IF BARCODE IN GOOD LIST, OUTPUT RECORD
+
+                # IF SINGLETONS AND BC IN SINGLETON LIST, OUTPUT TO OTHER FILE
 
 
+    else:
+        from_ = which_linkedread(inputs[0])
+        bc_counts = count_barcodes_fq(from_, inputs[0], inputs[1])
+        linked = list(filter(lambda x: bc_counts[x] > 2, bc_counts.keys()))
+        if singletons:
+            _singles = list(filter(lambda x: bc_counts[x] <= 2, bc_counts.keys()))
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog='separate_singletons',
-        description='Isolate singleton and non-singleton linked-read BAM records into separate files.',
-        usage = "separate_singletons -t threads -b barcode_tag -s singletons.bam input.bam > output.bam",
-        )
-    parser.add_argument("-b", dest = "bx_tag", metavar = "barcode_tag", type=str, default = "BX", help="The header tag with the barcode (default: %(default)s)")
-    parser.add_argument("-s", dest = "singletons", metavar = "singletons_file", type=str, default = "singletons.bam", help="Name of output singleton file (default: %(default)s)")
-    parser.add_argument("-t", dest = "threads", metavar="threads", type=int, default = 4, help="Number of threads to use (default: %(default)s)")
-    parser.add_argument('input', type = str, help = "Input bam file")
-    if len(sys.argv) == 1:
-        parser.print_help(sys.stderr)
-        sys.exit(1)
+        with (
+            pysam.FastxFile(inputs[0], persist=False) as R1,
+            pysam.FastxFile(inputs[1], persist=False) as R2,
+            open(f"{prefix}.R1.fq.gz", "wb") as R1_out,
+            open(f"{prefix}.R2.fq.gz", "wb") as R2_out,
+            subprocess.Popen("gzip", stdout= R1_out, stdin=subprocess.PIPE) as gz_r1,
+            subprocess.Popen("gzip", stdout= R2_out, stdin=subprocess.PIPE) as gz_r2,
+            open(f"{prefix}.singleton.R1.fq.gz", "wb") as R1_single,
+            open(f"{prefix}.singleton.R2.fq.gz", "wb") as R2_single,
+            subprocess.Popen("gzip", stdout= R1_single, stdin=subprocess.PIPE) as gz_r1_single,
+            subprocess.Popen("gzip", stdout= R2_single, stdin=subprocess.PIPE) as gz_r2_single,
+        ):
+            writer = CachedWriter(gz_r1, gz_r2, cache_size)
+            writer_inv = CachedWriter(gz_r1_single, gz_r2_single, cache_size)
 
-    args = parser.parse_args()
-    if args.threads <1:
-        parser.error(f"Threads supplied to -t ({args.threads}) must be positive (e.g. >=1)")
-    if not os.path.exists(args.input):
-        parser.error(f"{args.input} was not found")
-    if len(args.bx_tag) != 2 or not args.bx_tag.isalnum():
-        parser.error(f"The header tag supplied to -b ({args.bx_tag}) must be alphanumeric and exactly two characters long")
-
-    invalid_pattern = re.compile(r'[AaBbCcDd]00')
-    sorted_bam = f"{args.input[:-4]}.bxsort.bam"
-    result = subprocess.run(f"samtools sort -@ {args.threads} -o {sorted_bam} -t {args.bx_tag} {args.input}".split(), stderr=sys.stderr)
-    if result.returncode != 0:
-        sys.stderr.write(f"Error: samtools sort failed with exit code {result.returncode}\n")
-        sys.exit(1)
-    with (
-        pysam.AlignmentFile(sorted_bam, "rb", check_sq=False) as infile,
-        pysam.AlignmentFile(sys.stdout, "wb", template=infile) as nonsingleton,
-        pysam.AlignmentFile(args.singletons, "wb", template=infile) as singleton,                      
-    ):
-        record_store = []
-        read_count = 0
-        last_barcode = None
-        for record in infile:
-            try:
-                barcode = record.get_tag(args.bx_tag)
-                if isinstance(barcode, int):
-                    pass # an int from an MI-type tag
-                elif invalid_pattern.search(barcode):
-                    continue
-            except KeyError:
-                continue
-            # write the stored records when the barcode changes
-            if last_barcode and barcode != last_barcode:
-                target_file = nonsingleton if read_count > 1 else singleton  
-                for record in record_store:  
-                    target_file.write(record)  
-
-                # reset the record store and read count
-                record_store = []
-                read_count = 0
-
-            record_store.append(record)
-            if record.is_forward:
-                # +1 for a forward read, whether it is paired or not
-                read_count += 1
-            elif record.is_reverse and not record.is_paired:
-                # +1 for reverse only if it's unpaired, so the paired read doesn't count twice
-                read_count += 1
-            # update the last barcode with the current one
-            last_barcode = barcode
-        # After the for loop ends
-        if record_store:
-            target_file = nonsingleton if read_count > 1 else singleton
-            for i in record_store:
-                target_file.write(i)
-
-    # final housekeeping to remove intermediate
-    os.remove(sorted_bam)
