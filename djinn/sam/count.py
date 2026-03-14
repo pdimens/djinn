07268cea9a6f3d30a683e0310105e6bd7f720f2e
pdimens <pdimens@gmail.com>
2026-03-14 00:09:02 -0400

    add invalid

--- djinn/sam/count.py
+++ djinn/sam/count.py
@@ -3,14 +3,16 @@ import pysam
 import rich_click as click
 import sys
 from djinn.utils.file_ops import validate_sam
+from djinn.utils.barcodes import ANY_INVALID
 import signal
 if hasattr(signal, "SIGPIPE"):
     signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
 @click.command(no_args_is_help = True, context_settings={"allow_interspersed_args" : False}, epilog = "Documentation: https://pdimens.github.io/djinn/extract")
+@click.option("-i", "--invalid", is_flag=True, default=False, help = "Include invalid barcodes")
 @click.argument('input', required=True, type=click.Path(exists=True, readable=True, dir_okay=False, resolve_path=True), callback = validate_sam)
 @click.help_option('--help', hidden = True)
-def count(input):
+def count(input, invalid):
     '''
     Get counts for unique barcodes
     
@@ -23,10 +25,13 @@ def count(input):
             if not record.has_tag("BX"):
                 continue
             _bc = record.get_tag("BX")
+            is_invalid = ANY_INVALID.search(_bc)
             if record.is_read1:
-                barcodes.update([_bc])
+                if not is_invalid or (is_invalid and invalid):
+                    barcodes.update([_bc])
             elif record.is_read2 and (not record.is_paired or _bc not in barcodes):
-                barcodes.update([_bc])
+                if not is_invalid or (is_invalid and invalid):
+                    barcodes.update([_bc])
 
     for k,v in barcodes.items():
         sys.stdout.write(f"{k}\t{v}\n")
